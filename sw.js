135c15e75853de51381bb05352301fea5173c90e
Asia <joastr1@gmail.com>
2018-05-03 13:20:50 +0200

    sw cache implementation

--- sw.js
+++ sw.js
@@ -1,1 +1,53 @@
-// JavaScript Document
+
+const CACHE_NAME = 'my-restaurant-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'css/styles.css',
+  'data/restaurants.json',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+     
+        if (response) {
+          return response;
+        }
+
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+ 
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
